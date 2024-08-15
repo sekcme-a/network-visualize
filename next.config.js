92fb71968a9dc9eb20ca3cca687501aaf93c7a8c
송현서 <songmin21s@songhyeonseoui-MacBookPro.local>
2024-08-15 15:35:19 +0900

    asdf

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath:'/network-visualization'
+  // assetPrefix:
+  //   process.env.NODE_ENV === "production"
+  //     ? "https://sekcme-a.github.io/network-visualization"
+  //     : "",
+};
+
+export default nextConfig;
