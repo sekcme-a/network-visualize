659347aa325f7cda3f76f7ef77497d430973921e
송현서 <songmin21s@songhyeonseoui-MacBookPro.local>
2024-08-15 15:27:54 +0900

    sd

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,10 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
- 
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
- 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
- 
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
-}
- 
-module.exports = nextConfig
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "https://깃허브아이디.github.io/레파지토리이름"
+      : "",
+};
+
+export default nextConfig;
