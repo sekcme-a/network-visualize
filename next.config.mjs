4c4394922c70f830fdcf595858acff6e92c48d43
송현서 <songmin21s@songhyeonseoui-MacBookPro.local>
2024-08-15 15:42:15 +0900

    asdfg

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  output:'export',
   basePath: "/network-visualization",
 };
-
-module.exports = nextConfig;
+/* module.exports = nextConfig*/
+export default nextConfig;
